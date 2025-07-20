8ed338436befe452fadb3d0ee312e6a601272ada
lucas-brdt268 <lucas.brdt268@gmail.com>
2025-07-20 05:00:25 +0300

    maskgen

---
+++ include/maskgen.php
@@ -0,0 +1,16 @@
+<?php
+require_once "./include/config.php";
+
+function maskgen($imagePath)
+{
+    global $REPLICATE_API_KEY;
+
+    $apiKey = $REPLICATE_API_KEY;
+
+    $data = [
+        'version' => 'dbdded75163d60f68a1fda6a6bb58e458e0226c8b9fe1c259f09d6f91099fdb6',
+        'input' => [
+            ''
+        ]
+    ];
+}
